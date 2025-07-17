04bdb48cc4c152c10ae99bc008b2c63c140691ff
RomaLzhih <791191656@qq.com>
2025-07-17 15:57:06 -0700

    update dzig_run_scripts

--- dzig_scripts/run_dzig.sh
+++ dzig_scripts/run_dzig.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-prefix="/data/graphs/bin/"
-graphs=("com-orkut_sym.bin" "soc-LiveJournal1_sym.bin" "twitter_sym.bin" "friendster_sym.bin")
+# graphs=("com-orkut_sym.bin" "soc-LiveJournal1_sym.bin" "twitter_sym.bin" "friendster_sym.bin")
+# Map a graph name to its batch file path
+declare -A graph_map
+graph_map["soc-LiveJournal1_sym.bin"]="/data/zmen002/graph/byo/livejournal_batches/"
 # solvers=("run_std_set" "run_vector_vector" "run_ppcsr")
 solvers=("run_std_set")
+batch_size_seq=(1 10 100 1000 10000 100000 1000000)
+batch_num=10
+algorithm=("bfs" "pagerank")
 log="run_dzig_local.log"
+graph_path_prefix="/data/graphs/bin/"
 
 : >${log}
 for s in "${solvers[@]}"; do
   bazel build //benchmarks/run_structures:${s}
-  echo "Running ${s}" | tee -a ${log}
-  for g in "${graphs[@]}"; do
-    echo "${g}" | tee -a ${log}
-    path=${prefix}${g}
-    ./../bazel-bin/benchmarks/run_structures/${s} -s -b -i 1 -src 10 ${path} 2>&1 | tee -a ${log}
-    echo "*****" | tee -a ${log}
+  for g in "${!graph_map[@]}"; do
+    path=${graph_path_prefix}${g}
+
+    for batch_size in "${batch_size_seq[@]}"; do
+      # get the batch file path
+      batch_file="${graph_map[${g}]}batch_${batch_size}.in"
+
+      for a in "${algorithm[@]}"; do
+        echo ">>>Running ${s} on ${g} with batch size ${batch_size} and algorithm ${a}" | tee -a ${log}
+
+        ./../bazel-bin/benchmarks/run_structures/${s} -alg ${a} -batch_num ${batch_num} -batch_size ${batch_size} -batch_file ${batch_file} -s -b -i 1 -src 10 ${path} 2>&1 | tee -a ${log}
+
+        echo ">>>Program Finish." | tee -a ${log}
+
+      done
+    done
   done
   echo "-----------------------------------" | tee -a ${log}
 done
