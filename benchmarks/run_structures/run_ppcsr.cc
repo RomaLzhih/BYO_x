27b596e5280d48711e1cde4ac9d90661c93bfb12
ziyang <791191656@qq.com>
2025-07-24 19:40:34 -0700

    add copy operator for ppcsr

--- benchmarks/run_structures/run_ppcsr.cc
+++ benchmarks/run_structures/run_ppcsr.cc
@@ -12,8 +12,11 @@
 //     -s : indicate that the graph is symmetric
 //     -d : dump the output arrays to files, useful for debugging
 
+#include <cassert>
 #include <cstddef>
 #include <functional>
+
+#include "parlay/parallel.h"
 
 #ifdef HOMEGROWN
 #define PARLAY 1
@@ -179,19 +182,58 @@ struct symmetric_ppcsr_graph {
   //   */
   // }
   // copy assignment
-  // symmetric_ppcsr_graph &operator=(symmetric_ppcsr_graph &other) noexcept {
-  //   nodes = other.nodes;
-  //   if (other.vertex_weights != nullptr) {
-  //     vertex_weights = gbbs::new_array_no_init<vertex_weight_type>(N());
-  //     gbbs::parallel_for(0, N(), [&](size_t i) {
-  //       vertex_weights[i] = other.vertex_weights[i];
-  //     });
-  //   }
+  // symmetric_ppcsr_graph& operator=(symmetric_ppcsr_graph& other) noexcept {
+  //   auto edges = other.nodes.get_edges();
+  //   auto n = other.nodes.get_n();
+  //   auto m = other.nodes.num_edges();
+  //   nodes = decltype(nodes)(n);
+  //   gbbs::sequence<uint32_t> srcs =
+  //   gbbs::sequence<uint32_t>::uninitialized(m); gbbs::sequence<uint32_t>
+  //   dests = gbbs::sequence<uint32_t>::uninitialized(m);
+  //   gbbs::sequence<uint32_t> degrees(n, 0);
+  //   gbbs::sequence<uint8_t> all_true(m, 1);
+  //   parlay::parallel_for(0, edges.size(), [&](size_t i) {
+  //     srcs[i] = std::get<0>(edges[i]);
+  //     dests[i] = std::get<1>(edges[i]);
+  //   });
+  //   nodes.build_from_edges(srcs.data(), dests.data(), all_true.data(), n, m,
+  //                          degrees.data());
   //   return *this;
-  //   // other.vertex_weights = nullptr;
   // }
 
+  symmetric_ppcsr_graph& operator=(symmetric_ppcsr_graph& other) noexcept {
+    nodes = other.nodes;
+    if (other.vertex_weights != nullptr) {
+      vertex_weights = gbbs::new_array_no_init<vertex_weight_type>(N());
+      gbbs::parallel_for(0, N(), [&](size_t i) {
+        vertex_weights[i] = other.vertex_weights[i];
+      });
+    }
+    return *this;
+    // other.vertex_weights = nullptr;
+  }
+
   // Copy constructor
+
+  symmetric_ppcsr_graph(symmetric_ppcsr_graph& other)
+      : nodes(other.nodes.get_n()),
+        vertex_weights(other.vertex_weights),
+        deletion_fn(other.deletion_fn) {
+    auto edges = other.nodes.get_edges();
+    auto n = other.nodes.get_n();
+    auto m = other.nodes.num_edges();
+    gbbs::sequence<uint32_t> srcs = gbbs::sequence<uint32_t>::uninitialized(m);
+    gbbs::sequence<uint32_t> dests = gbbs::sequence<uint32_t>::uninitialized(m);
+    gbbs::sequence<uint32_t> degrees(n, 0);
+    gbbs::sequence<uint8_t> all_true(m, 1);
+    // BUG: this does not handle the weight
+    parlay::parallel_for(0, edges.size(), [&](size_t i) {
+      srcs[i] = std::get<0>(edges[i]);
+      dests[i] = std::get<1>(edges[i]);
+    });
+    nodes.build_from_edges(srcs.data(), dests.data(), all_true.data(), n, m,
+                           degrees.data());
+  }
   // symmetric_ppcsr_graph(const symmetric_ppcsr_graph &other) :
   // nodes(other.N()) {
   //   // printf("copy constructor\n");
